ee8d2fa6e7ea144540fc2c7d0d8fe119ba3b8514
Daniel Spinola <51745663+danspinola@users.noreply.github.com>
2020-11-28 16:17:04 -0300

    finish user cosmos

---
+++ AzureCosmos/AppSettings.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace AzureCosmos
+{
+    public class AppSettings
+    {
+        public string StorageConnectionString { get; set; }
+
+        public static AppSettings LoadAppSettings()
+        {
+            IConfigurationRoot configRoot = new ConfigurationBuilder()
+                .AddJsonFile("Settings.json")
+                .Build();
+
+            AppSettings appSettings = configRoot.Get<AppSettings>();
+            return appSettings;
+        }
+    }
+}
---
+++ AzureCosmos/Common.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using System;
+using System.Threading.Tasks;
+
+namespace AzureCosmos
+{
+    public class Common
+    {
+        public static CloudStorageAccount CreateStorageAccount(string storageConnectionString)
+        {
+            CloudStorageAccount storageAccount;
+            try
+            {
+                storageAccount = CloudStorageAccount.Parse(storageConnectionString);
+            }
+            catch (Exception e)
+            {
+                throw e;
+                throw;
+            }
+            return storageAccount;
+        }
+
+        public static async Task<CloudTable> CreateTableAsync(string tableName)
+        {
+            string storageConnection = AppSettings.LoadAppSettings().StorageConnectionString;
+
+            // Recuperar dados
+            CloudStorageAccount storageAccount = CreateStorageAccount(storageConnection);
+            // Criar um cliente para as tabelas
+            CloudTableClient tableClient = storageAccount
+                .CreateCloudTableClient(new TableClientConfiguration());
+            // Criar uma tabela 
+            CloudTable table = tableClient.GetTableReference(tableName);
+
+            bool created = await table.CreateIfNotExistsAsync();
+            return table;
+        }
+    }
+}
